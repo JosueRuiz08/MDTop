1bdccfeb85b26561f32c609e3229a7da4e3acb37
JosueRuiz08 <josue.ruiz>
2022-10-25 13:43:27 -0500

    UX NavigationIcon

--- app/src/main/java/com/alain/cursos/top/DetalleActivity.java
+++ app/src/main/java/com/alain/cursos/top/DetalleActivity.java
@@ -20,6 +20,7 @@ import android.widget.EditText;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
@@ -139,23 +140,24 @@ public class DetalleActivity extends AppCompatActivity {
 
         toolbarLayout.setExpandedTitleColor(Color.WHITE);
         appBar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
-            toolbarLayout.setTitle("appBarLayout = " + appBarLayout.getTotalScrollRange());
-            etNombre.setText("verticalOffset = "+ verticalOffset);
+//            toolbarLayout.setTitle("appBarLayout = " + appBarLayout.getTotalScrollRange());
+//            etNombre.setText("verticalOffset = "+ verticalOffset);
 //            if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()){
 //                toolbar.getNavigationIcon().setTint(Color.BLACK);
 //            }else{
 //                toolbar.getNavigationIcon().setTint(Color.WHITE);
 //            }
-            float percentage = Math.abs((float)Math.abs(verticalOffset)/appBarLayout.getTotalScrollRange() - 1);
-            int colorValue = (int) (percentage * 255);
-            if (toolbar.getNavigationIcon() != null) {
-                toolbar.getNavigationIcon().setTint(Color.rgb(colorValue,colorValue,colorValue));
+            if (AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_YES) {
+                float percentage = Math.abs((float) Math.abs(verticalOffset) / appBarLayout.getTotalScrollRange() - 1);
+                int colorValue = (int) (percentage * 255);
+                if (toolbar.getNavigationIcon() != null) {
+                    toolbar.getNavigationIcon().setTint(Color.rgb(colorValue, colorValue, colorValue));
+                }
+
+//                etApellidos.setText("VerticalScrool/appbar = " + (float) Math.abs(verticalOffset) / appBarLayout.getTotalScrollRange());
+//                etLugarNacimiento.setText("percentage = " + percentage);
+//                etNotas.setText("ColorValue = " + colorValue);
             }
-
-
-            etApellidos.setText("VerticalScrool/appbar = "+ (float)Math.abs(verticalOffset)/appBarLayout.getTotalScrollRange());
-            etLugarNacimiento.setText("percentage = "+ percentage);
-            etNotas.setText("ColorValue = " + colorValue);
 
         });
 
@@ -193,8 +195,14 @@ public class DetalleActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.action_save) {
-            saveOrEdit();
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_save:
+                saveOrEdit();
+                break;
         }
         return super.onOptionsItemSelected(item);
     }
